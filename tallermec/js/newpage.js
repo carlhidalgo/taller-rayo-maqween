8bc2171efa464a7b46189b6e4d2356f2fc7f2c14
carlhidalgo <106288856+carlhidalgo@users.noreply.github.com>
2024-04-29 00:49:10 -0400

    mejoras

--- tallermec/js/newpage.js
+++ tallermec/js/newpage.js
@@ -14,39 +14,60 @@ $(window).on('load', function(){
 	
 ////////////////// contacto///////////////////////////	
 	
-		function validateFormCotacto() {
-				var name = document.getElementById('name').value;
-				var email = document.getElementById('email').value;
-				var phone = document.getElementById('phone').value;
-				var message = document.getElementById('message').value;
-				if(name == "" || email == "" || phone == "" || message == "") {
+		
+			function validateFormCotacto() {
+				var name = $('#name').val();
+				var email = $('#email').val();
+				var phone = $('#phone').val();
+				var message = $('#message').val();
+				if (name == "" || email == "" || phone == "" || message == "") {
 					alert("Todos los campos deben estar llenos");
 					return false;
 				}
-			
+
 				return true;
 			}
-	
-	
+		
 ////////////////// contacto///////////////////////////	
 	
 function validateFormReserva() {
-	var nombre = document.getElementById("nombreInput").value;
-	var apellidos = document.getElementById("apellidosInput").value;
-	var rut = document.getElementById("rutInput").value;
-	var email = document.getElementById("emailInput").value;
-	var telefono = document.getElementById("telefonoInput").value;
-	var marca = document.getElementById("marcaInput").value;
-	var anio = document.getElementById("anioSelect").value;
+	var nombre = $("#nombreInput").val();
+	var apellidos = $("#apellidosInput").val();
+	var rut = $("#rutInput").val();
+	var email = $("#emailInput").val();
+	var telefono = $("#telefonoInput").val();
+	var marca = $("#marcaInput").val();
+	var anio = $("#anioSelect").val();
+	
 
 	if (nombre == "" || apellidos == "" || rut == "" || email == "" || telefono == "" || marca == "" || anio == "Seleccione año del vehiculo") {
 		alert("Por favor, complete todos los campos del formulario.");
 		return false;
 	}
 
+	// Validación de largo
+	if (nombre.length >= 3 || nombre.length > 21) {
+		alert("El nombre debe tener entre 3 y 20 caracteres.");
+		return false;
+	}
+
+	if (apellidos.length >= 3 || apellidos.length > 21) {
+		alert("El apellido debe tener entre 3 y 20 caracteres.");
+		return false;
+	}
+	if ( rut.length < 11 || rut.length > 8) {
+		alert("El rut debe tener entre 8 y 10 caracteres.");
+		return false;
+	}
+
+	if (telefono.length >= 9 || telefono.length > 12) {
+		alert("El teléfono debe tener entre 9 y 12 caracteres.");
+		return false;
+	}
+	
 	// Validación de formato de correo electrónico
 	var emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if (!email.match(emailFormat)) {
+	if (!email.match(emailFormat) ) {
 		alert("Por favor, ingrese un correo electrónico válido.");
 		return false;
 	}
@@ -58,7 +79,13 @@ function validateFormReserva() {
 		return false;
 	}
 
+	// Validación de formato de RUT
+	
 	return true;
+
+	
+
+
 }
 
 //////////////////barra de contacto///////////////////////////	
