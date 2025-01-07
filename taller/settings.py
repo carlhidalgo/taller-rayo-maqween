e306e9d43949ee8ea216f8d637d788d019c5d949
Carlos Hidalgo <106288856+carlhidalgo@users.noreply.github.com>
2025-01-06 22:53:07 -0300

    Add static files and update settings for production deployment

--- taller/settings.py
+++ taller/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp/static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directorio para los archivos estáticos en producción
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
