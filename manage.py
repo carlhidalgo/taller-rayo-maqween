78fb38e04e24027001ce4077c2179effe8e4cff0
Carlos Hidalgo <106288856+carlhidalgo@users.noreply.github.com>
2025-01-06 22:26:55 -0300

    Refactor Vercel configuration to use vercel_app.py and update manage.py for app handler

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from myapp import app
 import os
 import sys
 
+handler = app
 
 def main():
     """Run administrative tasks."""
